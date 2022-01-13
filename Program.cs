137a687991ab6ce8351e42c441b9fe01efad4535
dagremont@mail.ru <dagremont@mail.ru>
2022-01-13 13:05:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using задание_10;
+
+namespace задание_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ugol ugol = new Ugol();
+         
+
+
+            Console.WriteLine(ugol.gradus);
+            Console.WriteLine(ugol.min);
+            Console.WriteLine(ugol.sec);
+            Console.WriteLine(ugol.ToRadians()) ;
+
+            ugol.gradus = 15;
+            ugol.sec = 25;
+            ugol.min = 10;
+            Console.WriteLine(ugol.ToRadians());
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ Ugol.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_10
+{
+    class Ugol
+    {
+        private int gradusf=5;
+        private int minf=4;
+        private int secf=3;
+
+
+        public int gradus 
+        {
+            get 
+            {
+                return gradusf;
+            }
+            set 
+            {
+
+                if (value.GetType() == typeof(int))
+                    gradusf = value;
+               
+                else
+                    gradusf = 0;
+            }
+        }
+  
+
+        public int min
+        {
+            get
+            {
+                return minf;
+            }
+            set 
+            {
+
+                if (value.GetType() == typeof(int))
+                    minf = value;
+
+                else
+                    minf = 0;
+            }
+        }
+        public int sec
+        {
+            get
+            {
+                return secf;
+            }
+            set 
+            {
+
+                if (value.GetType() == typeof(int))
+                    secf = value;
+
+                else
+                    secf = 0;
+            }
+        }
+        public double ToRadians()
+        {
+
+  
+            return gradus * Math.PI / 180; 
+        }
+    }
+}
